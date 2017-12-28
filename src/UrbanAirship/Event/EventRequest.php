35aad6b1e3e06448c2ac44c5f1f5d509998662bd
Lars Hopman <l.k.hopman@gmail.com>
2017-12-28 14:22:23 +0100

    GEN-364 use correct content type

--- src/UrbanAirship/Event/EventRequest.php
+++ src/UrbanAirship/Event/EventRequest.php
@@ -142,7 +142,7 @@ class EventRequest implements RequestInterface
         $logger = UALog::getLogger();
 
         $response = $this->airship->request("POST",
-            json_encode($this->getPayload()), $uri, "application/vnd.urbanairship+json", 3 , null, true);
+            json_encode($this->getPayload()), $uri, "application/json", 3 , null, true);
 
         $logger->info("Event sent successfully.");
         return new EventResponse($response);
